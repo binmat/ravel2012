5edb197f0937ff5a08c722a3c09d11ff7dcd8377
lei <lei@savu>
2018-04-24 15:34:06 +0200

    Some modifications for manpages

--- R/ravel2012.R
+++ R/ravel2012.R
@@ -1,11 +1,14 @@
-#' @title ravel_2012
-#' @description Consists high-throughput profiling data collected from vaginal microbiota samples collected from women, who were not pregnant
-#' , of reproductive age, ranging from 12 to 45 years, regularly menstruating, with a history of sexual activity, and had not taken any antibiotic or antimycotic compounds in the past 30 days.
-#' @name ravel_2012
+#' @title ravel2012
+#' @description Ravel 2012 vaginal microbiota data set.
+#' @details This data set contains high-throughput profiling data of the vaginal microbiota collected from reproductive-age women (12-45 years). Exclusion criteria included pregnancy, irregular menstruations, sexual activity, and antibiotic or antimycotic compounds in the past 30 days. See the original publication by Ravel et al. (2012) for further details. We have organized the original public data set into a phyloseq object and prepared the R data package to facilitate the reuse of this data resource. In particular, we expect that it will be useful for scientists who develop methods to model the dynamics of microbial community profiling data.
+#' @name ravel2012
 #' @docType data
 #' @author Binu Mathew \email{binmat@utu.fi}
-#' @references Ravel J, Gajer P, Abdo Z, Schneider GM, Koenig SS, McCulle SL, et al. Vaginal microbiome of reproductive-age women. Proc Natl Acad Sci U S A (2011) 108(Suppl 1):4680–7. doi:10.1073/pnas.1002611107
-#' @usage ravel2012<-ravel2012
+#' @references Ravel J, Gajer P, Abdo Z, Schneider GM, Koenig SS, McCulle SL, et al. Vaginal microbiome of reproductive-age women. Proc Natl Acad Sci U S A (2011) 108(Suppl 1):4680–7. \url{https://doi.org/10.1073/pnas.1002611107}
+#' @usage
+#'   library(ravel2012)
+#'   data(ravel2012)
+#'   print(ravel2012)
 #' @return Data set as a \code{\link{phyloseq-class}} object
 #' @keywords data
 NULL
